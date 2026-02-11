f7baa244553c993197768e3a2a30dc398cd6fa64
noor <nooricorn.55@gmail.com>
2026-02-11 22:47:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+// Elements
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text"); 
+
+const initialState = document.getElementById("initial-state");
+const normalState = document.getElementById("normal-state");
+
+// Click Envelope
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+
+    // Transition from initial state to normal state after 2-3 seconds
+    setTimeout(() => {
+        initialState.style.display = "none";
+        normalState.style.display = "block";
+    }, 2500); // 2.5 seconds
+});
+
+// Logic to move the NO btn
+
+noBtn.addEventListener("mouseover", () => {
+    const min = 300;
+    const max = 300;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+// Logic to make YES btn to grow
+
+// let yesScale = 1;
+
+// yesBtn.style.position = "relative"
+// yesBtn.style.transformOrigin = "center center";
+// yesBtn.style.transition = "transform 0.3s ease";
+
+// noBtn.addEventListener("click", () => {
+//     yesScale += 2;
+
+//     if (yesBtn.style.position !== "fixed") {
+//         yesBtn.style.position = "fixed";
+//         yesBtn.style.top = "50%";
+//         yesBtn.style.left = "50%";
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }else{
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }
+// });
+
+// YES is clicked
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Yipeeee!";
+
+    catImg.src = "cat_dance.png";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+
+    // Switch to cat_end after 3 seconds
+    setTimeout(() => {
+        catImg.src = "cat_end.png";
+    }, 3000);
+});
